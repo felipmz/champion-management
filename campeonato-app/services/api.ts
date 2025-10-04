87729e77d59b166d3e920c6fd51762162a726790
Genisson Emilio <esgenisson@gmail.com>
2025-10-04 10:09:53 -0300

    Update api.ts

--- campeonato-app/services/api.ts
+++ campeonato-app/services/api.ts
@@ -1,7 +1,7 @@
 // services/api.ts
 import axios from 'axios';
 
-const API_URL = 'https://champion-management.vercel.app/api';
+const API_URL = 'https://champio-backend.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
