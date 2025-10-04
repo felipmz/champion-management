8d7e399f1081836ed7fc40131cfd0c8e3cbd257b
Genisson Emilio <esgenisson@gmail.com>
2025-10-04 09:56:09 -0300

    descendoooooooooooo

--- campeonato-api/models/Team.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const teamSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  championship_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Championship', required: true },
-}, { timestamps: true });
-module.exports = mongoose.model('Team', teamSchema);
